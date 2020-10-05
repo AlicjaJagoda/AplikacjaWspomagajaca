d82bc8ef52ffbf060d21f14b37f1da562e415b97
unknown <alicjajagoda1997@gmail.com>
2020-10-05 14:17:28 +0200

    Interfejs dodanie prostego guzika odnoszacego do aktywnosci

--- app/src/main/java/com/example/aplikacjawspomagajaca/MainActivity.java
+++ app/src/main/java/com/example/aplikacjawspomagajaca/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.aplikacjawspomagajaca;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnZeskanujKod;
+    Intent aktSkanowanieIntent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnZeskanujKod=(Button) findViewById(R.id.btnZeskanujKod);
+        btnZeskanujKod.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nowaAkt();
+            }
+        });
+    }
+    public void nowaAkt(){
+        aktSkanowanieIntent= new Intent(this, SkanowanieActivity.class);
+        startActivity(aktSkanowanieIntent);
     }
 }
