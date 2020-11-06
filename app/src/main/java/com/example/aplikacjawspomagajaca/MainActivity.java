cac050a513fb422d19be5a5be6f16f5fc4275573
unknown <alicjajagoda1997@gmail.com>
2020-11-06 12:54:20 +0100

    Dodanie uprawienia do uzytku kamery, zapytanie uzytkownika

--- app/src/main/java/com/example/aplikacjawspomagajaca/MainActivity.java
+++ app/src/main/java/com/example/aplikacjawspomagajaca/MainActivity.java
@@ -1,19 +1,26 @@
 package com.example.aplikacjawspomagajaca;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int PERMISSION_REQUEST_CAMERA = 0;
     Button btnZeskanujKod;
     Intent aktSkanowanieIntent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestCamera();
         btnZeskanujKod=(Button) findViewById(R.id.btnZeskanujKod);
         btnZeskanujKod.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -21,7 +28,33 @@ public class MainActivity extends AppCompatActivity {
                 nowaAkt();
             }
         });
+
     }
+    private void requestCamera() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            startCamera();
+        } else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CAMERA);
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CAMERA);
+            }
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST_CAMERA) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startCamera();
+            } else {
+                Toast.makeText(this, "Camera Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    private void startCamera() {
+        //todo
+    }
+
     public void nowaAkt(){
         aktSkanowanieIntent= new Intent(this, SkanowanieActivity.class);
         startActivity(aktSkanowanieIntent);
