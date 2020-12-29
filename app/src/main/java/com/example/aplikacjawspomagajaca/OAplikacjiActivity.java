2068da242dbc7a00a39afb6a0a96c1a6e235f4f6
unknown <alicjajagoda1997@gmail.com>
2020-12-29 22:16:45 +0100

    dodano aktywnosc informacyjna o autorze

---
+++ app/src/main/java/com/example/aplikacjawspomagajaca/OAplikacjiActivity.java
@@ -0,0 +1,14 @@
+package com.example.aplikacjawspomagajaca;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OAplikacjiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_o_aplikacji);
+    }
+}
